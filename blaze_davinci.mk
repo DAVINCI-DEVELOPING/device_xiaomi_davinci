210e71eacadf3240973aef50f2216cd37c75cb10
jessepincay <pincayjesse@gmail.com>
2024-06-20 15:35:41 -0500

    Welcome Project Blaze

--- blaze_davinci.mk
+++ blaze_davinci.mk
@@ -7,13 +7,18 @@
 # Inherit from davinci device
 $(call inherit-product, device/xiaomi/davinci/device.mk)
 
-# Inherit some common DerpFest stuff.
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
-DERP_BUILDTYPE := Official
-EXTRA_UDFPS_ICONS := true
+# Inherit some common Blaze stuff.
+$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
+
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_USES_PICO_GAPPS := true
+BLAZE_BUILD_TYPE := OFFICIAL
+BLAZE_MAINTAINER := Jezzay97
+WITH_GAPPS := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := derp_davinci
+PRODUCT_NAME := blaze_davinci
 PRODUCT_DEVICE := davinci
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
